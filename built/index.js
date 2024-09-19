730bac31a2f3757265d347f5f0698bbfde498c15
Rubén <rjulian@elpuig.xeill.net>
2024-09-19 20:38:03 +0200

    Funcion lista añadida

--- built/index.js
+++ built/index.js
@@ -1,2 +1,16 @@
 "use strict";
-alert("Hola");
+let titulo = document.getElementById("titulo");
+function cambiarTitulo() {
+    if (titulo) {
+        titulo.innerHTML = "Títuloooooooooooooooooooo";
+        titulo.style.color = "000f0f";
+    }
+}
+function addToList() {
+    let lista = document.getElementById("lista");
+    let nuevoElemento = document.createElement("li");
+    lista.append(nuevoElemento);
+    nuevoElemento.innerHTML = "Elemento " + contador;
+    contador++;
+}
+let contador = 1;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-let tittle: HTMLElement = document.getElementById("titulo");
+let titulo: HTMLElement = document.getElementById("titulo")!;
+
+function cambiarTitulo(){
+    if(titulo!){
+        titulo.innerHTML = "Títuloooooooooooooooooooo";
+        titulo.style.color = "000f0f"
+    }
+}
+
+function addToList(){
+    let lista: HTMLElement = document.getElementById("lista")!;
+    let nuevoElemento = document.createElement("li");
+    lista.append(nuevoElemento);
+    nuevoElemento.innerHTML = "Elemento "+ contador;
+    contador++;
+
+}
+let contador: number = 1;
