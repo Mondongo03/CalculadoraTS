a5e1edbbeddf67ae620075fffccc51d02accc58c
Rubén <rjulian@elpuig.xeill.net>
2024-09-30 20:49:25 +0200

    Lógica Terminadoa

--- built/index.js
+++ built/index.js
@@ -1,32 +1,4 @@
 "use strict";
-function addToList() {
-    let lista = document.getElementById("lista");
-    let nuevoElemento = document.createElement("li");
-    let content = document.getElementById("campotexto").value;
-    lista.append(nuevoElemento);
-    let defaultTipo = document.getElementById("default");
-    let boldTipo = document.getElementById("bold");
-    let italicTipo = document.getElementById("italic");
-    if (boldTipo.checked)
-        nuevoElemento.innerHTML = "<b>" + content + "</b>";
-    else if (italicTipo.checked)
-        nuevoElemento.innerHTML = "<i>" + content + "</i>";
-    else
-        nuevoElemento.innerHTML = content;
-    content != "";
-}
-function deleteToList() {
-    const lista = document.getElementById("lista");
-    const index = document.getElementById("campotexto").value;
-    const items = lista.getElementsByTagName("li");
-    const indexNum = parseInt(index, 10);
-    if (!isNaN(indexNum) && indexNum > 0 && indexNum <= items.length) {
-        lista.removeChild(items[indexNum - 1]);
-    }
-    else {
-        alert("Por favor, ingresa un número válido dentro del rango de elementos.");
-    }
-}
 let numero1 = "";
 let numero2 = "";
 let suma = false;
@@ -34,61 +6,44 @@ let multiplicacion = false;
 let resta = false;
 let division = false;
 let labelText = document.getElementById("texto");
-/*function clickNumber(numero:number){
+function clickNumber(numero) {
     if (typeof numero !== 'number') {
         console.error('El parámetro debe ser un número');
-        return; // Salir de la función si no es un número
+        return;
     }
-    switch(numero){
-        case 1: {
-            if(!suma || !multiplicacion || !resta || !division) numero1 = numero1.concat("1");
-            else numero2 = numero2.concat("1");
-            labelText!.innerText = labelText!.innerText + "1";
-                break;
-
+    /*if(!suma || !multiplicacion || !resta || !division) numero1 = numero1.concat(numero.toString());
+    else numero2 = numero2.concat(numero.toString());*/
+    labelText.innerText = labelText.innerText + numero;
+}
+function updateOperacion(operador) {
+    switch (operador) {
+        case "+": {
+            labelText.innerText = labelText.innerText + "+";
+            break;
         }
-       
-    }
-}*/
-let currentOperation = '';
-let currentInput = '';
-function clickNumber(num) {
-    currentInput += num.toString();
-    updateTexto(currentOperation + currentInput);
-}
-function updateTexto(value) {
-    const display = document.getElementById('texto');
-    if (display) {
-        display.innerText = value;
-    }
-}
-function clearTexto() {
-    currentOperation = '';
-    currentInput = '';
-    updateTexto('0');
-}
-function reset() {
-    currentOperation = '';
-    currentInput = '';
-}
-function updateOperacion(op) {
-    if (currentInput) {
-        currentOperation += currentInput + ' ' + op + ' ';
-        currentInput = '';
-        updateTexto(currentOperation);
+        case "-": {
+            labelText.innerText = labelText.innerText + "-";
+            break;
+        }
+        case "*": {
+            labelText.innerText = labelText.innerText + "*";
+            break;
+        }
+        case "/": {
+            labelText.innerText = labelText.innerText + "/";
+            break;
+        }
     }
 }
+/*
+suma = true;
+            resta = false;
+            multiplicacion = false;
+            division = false;
+*/
 function calcularResultado() {
-    if (currentInput) {
-        currentOperation += currentInput;
-        try {
-            const result = eval(currentOperation);
-            updateTexto(result.toString());
-            reset();
-        }
-        catch (error) {
-            updateTexto('Error');
-            reset();
-        }
-    }
+    labelText.innerText = eval(labelText.textContent);
+}
+function limpiarResultado() {
+    labelText.textContent = "";
 }
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,4 @@
 
-function addToList(){
-    let lista: HTMLElement = document.getElementById("lista")!;
-    let nuevoElemento = document.createElement("li");
-    let content  = (document.getElementById("campotexto") as HTMLInputElement).value;
-    lista.append(nuevoElemento);
-    let defaultTipo: HTMLInputElement = (document.getElementById("default")! as HTMLInputElement);
-    let boldTipo: HTMLInputElement = (document.getElementById("bold")!  as HTMLInputElement);
-    let italicTipo: HTMLInputElement = (document.getElementById("italic")!  as HTMLInputElement);
-
- 
-    if(boldTipo.checked)  nuevoElemento.innerHTML = "<b>"+content+"</b>";
-    else if(italicTipo.checked)  nuevoElemento.innerHTML = "<i>"+content+"</i>";
-    else nuevoElemento.innerHTML = content;
-    
-    content!= "" ;
-    
-
-}
-
-function deleteToList(): void {
-    const lista = document.getElementById("lista") as HTMLUListElement;
-    const index = (document.getElementById("campotexto") as HTMLInputElement).value;
-    const items = lista.getElementsByTagName("li");
-    const indexNum = parseInt(index, 10);
-
-    if (!isNaN(indexNum) && indexNum > 0 && indexNum <= items.length) {
-        lista.removeChild(items[indexNum - 1]);
-    } else {
-        alert("Por favor, ingresa un número válido dentro del rango de elementos.");
-    }
-}
 
 let numero1: string = "";
 let numero2: string = "";
@@ -39,66 +8,46 @@ let resta: boolean = false;
 let division: boolean = false;
 let labelText = document.getElementById("texto");
 
-/*function clickNumber(numero:number){
+function clickNumber(numero:number){
     if (typeof numero !== 'number') {
         console.error('El parámetro debe ser un número');
-        return; // Salir de la función si no es un número
+        return; 
     }
-    switch(numero){
-        case 1: {
-            if(!suma || !multiplicacion || !resta || !division) numero1 = numero1.concat("1");
-            else numero2 = numero2.concat("1");
-            labelText!.innerText = labelText!.innerText + "1"; 
-                break;
-
+    /*if(!suma || !multiplicacion || !resta || !division) numero1 = numero1.concat(numero.toString());
+    else numero2 = numero2.concat(numero.toString());*/
+    
+    labelText!.innerText = labelText!.innerText + numero; 
+    
+}
+function updateOperacion(operador:String) {
+    switch(operador){
+        case"+": {
+            labelText!.innerText = labelText!.innerText + "+";
+            break;
         }
-       
-    }
-}*/
-let currentOperation: string = '';
-let currentInput: string = '';
-
-function clickNumber(num: number): void {
-    currentInput += num.toString();
-    updateTexto(currentOperation + currentInput);
-}
-
-function updateTexto(value: string): void {
-    const display: HTMLElement | null = document.getElementById('texto');
-    if (display) {
-        display.innerText = value;
-    }
-}
-
-function clearTexto(): void {
-    currentOperation = '';
-    currentInput = '';
-    updateTexto('0');
-}
-
-function reset(): void {
-    currentOperation = '';
-    currentInput = '';
-}
-
-function updateOperacion(op: string): void {
-    if (currentInput) {
-        currentOperation += currentInput + ' ' + op + ' ';
-        currentInput = '';
-        updateTexto(currentOperation);
-    }
-}
-
-function calcularResultado(): void {
-    if (currentInput) {
-        currentOperation += currentInput;
-        try {
-            const result = eval(currentOperation); 
-            updateTexto(result.toString());
-            reset();
-        } catch (error) {
-            updateTexto('Error');
-            reset();
+        case"-": {
+            labelText!.innerText = labelText!.innerText + "-";
+            break;
+        }
+        case"*": {
+            labelText!.innerText = labelText!.innerText + "*";
+            break;
+        }
+        case"/": {
+            labelText!.innerText = labelText!.innerText + "/";
+            break;
         }
     }
+}
+/*
+suma = true;
+            resta = false;
+            multiplicacion = false;
+            division = false;
+*/
+function calcularResultado(){
+    labelText!.innerText = eval(labelText!.textContent!);
+}
+function limpiarResultado(){
+    labelText!.textContent = "";
 }
